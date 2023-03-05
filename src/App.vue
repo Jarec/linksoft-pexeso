c4e75fb4eb2fffc681b72ff1bad0aa32bce5ccac
Jaroslav Crhonek <jarek.crhonek.pv@gmail.com>
2023-03-05 20:47:24 +0100

    linksoft pexeso initial version

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<script setup>
+import PexesoCard from "@/PexesoCard.vue";
+import { computed, onMounted, ref } from "vue";
+import { useWindowSize } from "@vueuse/core";
+
+const { width, height } = useWindowSize();
+console.log("width: ", width);
+console.log("height: ", height);
+const adjustedWidth = computed(
+  () => Math.min(width.value-25, height.value-125) + "px"
+);
+
+const cards = ref([]);
+
+function reset() {
+  const pictureKeys = [
+    "bagr",
+    "brouk",
+    "dum",
+    "dvere",
+    "flaska",
+    "hrnicek",
+    "kondom",
+    "kouzelnik",
+    "lod",
+    "pes",
+    "pivo",
+    "pocitac",
+    "ryba",
+    "strom",
+    "vitr",
+    "vlasy",
+    "wc",
+    "zidle",
+  ];
+
+  const shuffleArray = (arr) =>
+    arr
+      .map((i) => ({ val: i, r: Math.random() }))
+      .sort((a, b) => a.r - b.r)
+      .map((i) => i.val);
+  const generatedCards = [];
+  for (let pictureKey of pictureKeys) {
+    let pictureCard = {
+      key: crypto.randomUUID(),
+      pictureKey: pictureKey,
+      pictureId: pictureKey + "_1",
+      solved: false,
+      peek: false,
+    };
+    generatedCards.push(pictureCard, {
+      ...pictureCard,
+      pictureId: pictureKey + "_2",
+      key: crypto.randomUUID(),
+    });
+  }
+  cards.value = shuffleArray(generatedCards);
+}
+const peekedCards = computed(() => cards.value.filter((i) => i.peek));
+
+function peek(card) {
+  if (card.peek || card.solved) {
+    return;
+  }
+
+  if (peekedCards.value.length >= 2) {
+    cards.value.forEach((i) => (i.peek = false));
+  }
+
+  card.peek = true;
+
+  if (
+    peekedCards.value.length === 2 &&
+    peekedCards.value[0].pictureKey === peekedCards.value[1].pictureKey
+  ) {
+    peekedCards.value.forEach((i) => (i.solved = true));
+  }
+}
+
+onMounted(() => reset());
+</script>
+
+<template>
+  <div class="app">
+    <div class="control">
+      <button @click="reset" style="text-align: right">Reset</button>
+    </div>
+    <div class="main">
+      <pexeso-card
+        v-for="card in cards"
+        :key="card.key"
+        :card="card"
+        @click="() => peek(card)"
+      >
+      </pexeso-card>
+    </div>
+    <div class="footer">
+      <hr />
+      © 2023 LinkSoft
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  width: v-bind(adjustedWidth);
+  /** centering */
+  margin: 0 auto;
+}
+
+.main {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 10px;
+}
+
+.control {
+  margin-bottom: 10px;
+  text-align: right;
+}
+
+.footer {
+  text-align: center;
+}
+
+.control button {
+  cursor: pointer;
+  outline: 0;
+  color: #fff;
+  background-color: #123054;
+  border-color: #123054;
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  text-align: center;
+  border: 1px solid transparent;
+  padding: 6px 12px;
+  font-size: 16px;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+.control button:hover {
+  color: #fff;
+  background-color: #0b5ed7;
+  border-color: #0a58ca;
+}
+</style>
---
+++ src/PexesoCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed, toRefs } from "vue";
+
+const props = defineProps({
+  card: {
+    type: Object,
+    required: true,
+  },
+});
+
+const { peek, pictureId, solved } = toRefs(props.card);
+
+const styleOverride = computed(() => {
+  if (solved.value) {
+    return {
+      "background-image": `url("${pictureId.value}.jpg")`,
+      "background-size": "cover",
+      "border-color": "green",
+    };
+  } else if (peek.value) {
+    return {
+      "background-image": `url("${pictureId.value}.jpg")`,
+      "background-size": "cover",
+      "border-color": "red",
+      transition: "all 0.4s ease",
+      transform: "rotateY(180deg)",
+    };
+  } else {
+    return {};
+  }
+});
+</script>
+
+<template>
+  <div class="card" :style="styleOverride" />
+</template>
+
+<style scoped>
+.card {
+  width: 100%;
+  aspect-ratio: 1;
+  border: 2px solid black;
+  border-radius: 5px;
+  background-image: url("background.jpg");
+  background-size: cover;
+}
+</style>
